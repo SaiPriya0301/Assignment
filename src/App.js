57a0be105f1391a9d1e98b0f7eadb3aacab5072c
Sai Priya <gspriya7337@gmail.com>
2024-02-22 15:21:57 +0530

    added local storage for playback time

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
-import { saveAudioFile, getAllAudioFiles } from './indexedDBUtil';
-import NowPlayingCard from './Components/NowPlayingCard';
-import { useEffect, useState } from 'react';
-import {AddButton } from './Components/AddButton';
-import { FaPlay } from 'react-icons/fa';
+import "./App.css";
+import { saveAudioFile, getAllAudioFiles } from "./indexedDBUtil";
+import NowPlayingCard from "./Components/NowPlayingCard";
+import { useEffect, useState } from "react";
+import { AddButton } from "./Components/AddButton";
+import { FaPlay } from "react-icons/fa";
+
 function App() {
   const [audioFiles, setAudioFiles] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(-1);
@@ -12,9 +13,11 @@ function App() {
     const fetchAudioFiles = async () => {
       const files = await getAllAudioFiles();
       setAudioFiles(files);
-      const lastAudioKey = localStorage.getItem('lastAudioKey');
+      const lastAudioKey = localStorage.getItem("lastAudioKey");
       if (lastAudioKey) {
-        const index = files.findIndex(file => file.id === parseInt(lastAudioKey, 10));
+        const index = files.findIndex(
+          (file) => file.id === parseInt(lastAudioKey, 10)
+        );
         if (index !== -1) {
           setCurrentIndex(index);
         }
@@ -23,11 +26,11 @@ function App() {
     fetchAudioFiles();
   }, []);
 
-  const handleFileUpload = async event => {
+  const handleFileUpload = async (event) => {
     const files = event.target.files;
     for (let file of files) {
       const fileData = await file.arrayBuffer();
-      await saveAudioFile(fileData, file.name); 
+      await saveAudioFile(fileData, file.name);
     }
     const updatedFiles = await getAllAudioFiles();
     setAudioFiles(updatedFiles);
@@ -36,42 +39,49 @@ function App() {
   const handleSelectAudio = (index) => {
     setCurrentIndex(index);
     const file = audioFiles[index];
-    localStorage.setItem('lastAudioKey', file.id.toString());
+    localStorage.setItem("lastAudioKey", file.id.toString());
   };
 
   const goToNextTrack = () => {
+    localStorage.setItem("audioPlaybackTime", 0);
+
     const nextIndex = (currentIndex + 1) % audioFiles.length;
     handleSelectAudio(nextIndex);
   };
 
   const goToPreviousTrack = () => {
-    const prevIndex = (currentIndex - 1 + audioFiles.length) % audioFiles.length;
+    localStorage.setItem("audioPlaybackTime", 0);
+    const prevIndex =
+      (currentIndex - 1 + audioFiles.length) % audioFiles.length;
     handleSelectAudio(prevIndex);
   };
-  
+
   return (
     <div className="flex flex-col h-screen p-4">
-      <div className='flex flex-col items-center w-[100%] md:flex-row'>
-      <AddButton handleFileUpload={handleFileUpload}></AddButton>
-      <div className='items-center'>
-     
-        <NowPlayingCard
-          currentFile={audioFiles[currentIndex]}
-          goToNextTrack={goToNextTrack}
-          goToPreviousTrack={goToPreviousTrack}
-        />
-      
-      </div>
+      <div className="flex flex-col items-center w-[100%] md:flex-row">
+        <AddButton handleFileUpload={handleFileUpload}></AddButton>
+        <div className="items-center">
+          <NowPlayingCard
+            currentFile={audioFiles[currentIndex]}
+            goToNextTrack={goToNextTrack}
+            goToPreviousTrack={goToPreviousTrack}
+          />
+        </div>
       </div>
       <div className="overflow-scroll hide-scrollbar h-[500px] mt-2">
         {audioFiles.map((file, index) => (
-          <div className='bg-teal-500 my-4 p-2 rounded flex flex-row items-center justify-evenly hover:border-cyan-900 border border-slate-300 cursor-pointer' key={file.id} onClick={() => handleSelectAudio(index)}>
-            <p className='w-[70%] items-center font-medium'>Audio {file.name}</p>
-            <FaPlay className='[30%] items-center'/>
+          <div
+            className="bg-teal-500 my-4 p-2 rounded flex flex-row items-center justify-evenly hover:border-cyan-900 border border-slate-300 cursor-pointer"
+            key={file.id}
+            onClick={() => handleSelectAudio(index)}
+          >
+            <p className="w-[70%] items-center font-medium">
+              Audio {file.name}
+            </p>
+            <FaPlay className="[30%] items-center" />
           </div>
         ))}
       </div>
-      
     </div>
   );
 }
--- src/Components/NowPlayingCard.js
+++ src/Components/NowPlayingCard.js
@@ -1,28 +1,79 @@
-import React from 'react';
+import React, { useEffect, useRef } from "react";
 import { FaMusic } from "react-icons/fa";
-import { FaBackward, FaForward } from 'react-icons/fa';
-const NowPlayingCard = ({ currentFile, goToNextTrack, goToPreviousTrack }) => {
+import { FaBackward, FaForward } from "react-icons/fa";
 
+const NowPlayingCard = ({ currentFile, goToNextTrack, goToPreviousTrack }) => {
+  const audioRef = useRef(null);
+
+  useEffect(() => {
+    // Ensure the audio element is present
+    if (audioRef.current) {
+      // Retrieve the stored playback time and parse it as a float
+      const savedTime = parseFloat(
+        localStorage.getItem("audioPlaybackTime") || 0
+      );
+      audioRef.current.currentTime = savedTime;
+
+      // Define the function to update the playback time in storage
+      const updatePlaybackTime = () => {
+        localStorage.setItem(
+          "audioPlaybackTime",
+          audioRef.current.currentTime.toString()
+        );
+      };
+
+      // Add event listener for time updates
+      audioRef.current.addEventListener("timeupdate", updatePlaybackTime);
+
+      // Add event listener for when the audio ends
+      audioRef.current.addEventListener("ended", goToNextTrack);
+
+      // Cleanup function to remove the event listeners
+      return () => {
+        if (audioRef.current) {
+          audioRef.current.removeEventListener(
+            "timeupdate",
+            updatePlaybackTime
+          );
+          audioRef.current.removeEventListener("ended", goToNextTrack);
+        }
+      };
+    }
+  }, [goToNextTrack]);
   return (
-    <div className='bg-cyan-700 items-center flex flex-col py-6 rounded-lg hover:border-teal-900 border w-[100%] px-14'>
-        <FaMusic size={100} />
-     {currentFile?   <div><p className='font-bold p-4 text-center'>{currentFile.name}</p>
-        <div className=' flex flex-row justify-center items-center'>
-      <button onClick={goToPreviousTrack} className="hover:bg-opacity-50 hover:rounded-full p-2 hover:bg-cyan-600"><FaBackward size={25}/></button>
-      <audio
-        controls
-        autoPlay
-        src={URL.createObjectURL(new Blob([currentFile.file]))}
-        onEnded={goToNextTrack}
-       className='mx-2'
-      >
-        Your browser does not support the audio element.
-      </audio>
-      <button onClick={goToNextTrack} className="hover:bg-opacity-50 hover:rounded-full p-2 hover:bg-cyan-600"><FaForward size={25}/></button>
-      </div>
-      </div> :
-        <p className='font-bold p-4'>No song currently playing</p>
-}
+    <div className="bg-cyan-700 items-center flex flex-col py-6 rounded-lg hover:border-teal-900 border w-[100%] px-14">
+      <FaMusic size={100} />
+      {currentFile ? (
+        <div>
+          <p className="font-bold p-4 text-center">{currentFile.name}</p>
+          <div className=" flex flex-row justify-center items-center">
+            <button
+              onClick={goToPreviousTrack}
+              className="hover:bg-opacity-50 hover:rounded-full p-2 hover:bg-cyan-600"
+            >
+              <FaBackward size={25} />
+            </button>
+            <audio
+              controls
+              ref={audioRef}
+              autoPlay
+              src={URL.createObjectURL(new Blob([currentFile.file]))}
+              onEnded={goToNextTrack}
+              className="mx-2"
+            >
+              Your browser does not support the audio element.
+            </audio>
+            <button
+              onClick={goToNextTrack}
+              className="hover:bg-opacity-50 hover:rounded-full p-2 hover:bg-cyan-600"
+            >
+              <FaForward size={25} />
+            </button>
+          </div>
+        </div>
+      ) : (
+        <p className="font-bold p-4">No song currently playing</p>
+      )}
     </div>
   );
 };
